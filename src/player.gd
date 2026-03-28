e92868a234f8130df17a5d1252d0e13a7c780dcb
Jardel <jardelguim23@gmail.com>
2026-03-28 08:21:09 -0300

    fix player jump

--- src/player.gd
+++ src/player.gd
@@ -10,11 +10,11 @@ signal player_hit
 func _physics_process(delta):
 	# rotaciona baseado na velocidaded
 	self.rotation = deg_to_rad(clamp(self.velocity.y * 0.1, -30, 90))
+	
 	if is_grav_on:
 		self.velocity.y += gravity * delta
-
-	if Input.is_action_just_pressed("jump"):
-		self.velocity.y = jump_force
+		if Input.is_action_just_pressed("jump"):
+			self.velocity.y = jump_force
 
 	var collision = move_and_collide(velocity * delta)
 	if collision:
