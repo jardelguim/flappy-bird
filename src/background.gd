e0c6f6836e147abc7bc067b4ea40c44052bf4763
Jardel <jardelguim23@gmail.com>
2026-04-02 08:53:39 -0300

    fix player sprite + background adjustments

--- src/background.gd
+++ src/background.gd
@@ -10,5 +10,5 @@ func _process(delta: float) -> void:
 
 func _scroll_background(delta : float):
 	clouds.screen_offset.x = lerp(clouds.screen_offset.x , clouds.screen_offset.x + 0.5 , 0.5)
-	forest.screen_offset.x = lerp(forest.screen_offset.x , forest.screen_offset.x + 1 , 0.5)
+	forest.screen_offset.x = lerp(forest.screen_offset.x , forest.screen_offset.x + 0.5 , 1)
 	
